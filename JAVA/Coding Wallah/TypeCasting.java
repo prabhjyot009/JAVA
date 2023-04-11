24795e4b7f1aba82da28c8d52f059f1e4bbb8ca5
prabhjyot009 <prabh156dps@gmail.com>
2023-04-11 14:52:15 +0530

    Coding Wallah

---
+++ JAVA/Coding Wallah/TypeCasting.java
@@ -0,0 +1,9 @@
+// Implicit TypeCasting:
+
+public class TypeCasting{
+    public static void main(String[] args) {
+        short sh=12;
+        int intData=sh;
+        System.out.println(intData);
+    }
+}
