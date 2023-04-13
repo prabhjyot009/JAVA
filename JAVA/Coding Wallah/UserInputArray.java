f62320384414518608cd546b3ecb84bdab28a1a1
prabhjyot009 <prabh156dps@gmail.com>
2023-04-13 22:08:08 +0530

    Java

--- JAVA/Coding Wallah/UserInputArray.java
+++ JAVA/Coding Wallah/UserInputArray.java
@@ -1,20 +1,33 @@
 import java.util.Scanner;
 public class UserInputArray {
-    public static void main(String[] args) {
-        //static array is an array jisme compile time se pehle he size daaldehnge ham.//size fixed
-        //int[] marks=new int[5];
-        //dynamic array jiska size grow karjata hai.//size grow
-        Scanner scan=new Scanner(System.in);
-        System.out.print("Enter the number of subjects:");
-        int size=scan.nextInt();
-        int[] marks=new int[size];
-        //user input in array
-        System.out.println("Enter marks of subjects:");
-        for(int i=0;i<size;i++){
-            marks[i]=scan.nextInt();
-        }
-        for(int i=0;i<size;i++){
-            System.out.print(marks[i]+",");
-        }
-}
+//     public static void main(String[] args) {
+//         //static array is an array jisme compile time se pehle he size daaldehnge ham.//size fixed
+//         //int[] marks=new int[5];
+//         //dynamic array jiska size grow karjata hai.//size grow
+//         Scanner scan=new Scanner(System.in);
+//         System.out.print("Enter the number of subjects:");
+//         int size=scan.nextInt();
+//         int[] marks=new int[size];
+//         //user input in array
+//         System.out.println("Enter marks of subjects:");
+//         for(int i=0;i<size;i++){
+//             marks[i]=scan.nextInt();
+//         }
+//         for(int i=0;i<size;i++){
+//             System.out.print(marks[i]+",");
+//         }
+// }
+
+public static void main(String[] args) {
+    System.out.println("Enter number of subjects:");
+    Scanner sc=new Scanner(System.in);
+    int size=sc.nextInt();
+    int[] marks=new int[size];
+    System.out.println("Enter marks of subjects");
+    for(int i=0;i<size;i++){
+        marks[i]=sc.nextInt();
+    }
+    for(int i=0;i<size;i++){
+        System.out.println("Marks:"+marks[i]);
+    }}
 }
