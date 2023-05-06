27df5e34285abe3df7fcb066e8c827ca8013ba2a
prabhjyot009 <prabh156dps@gmail.com>
2023-05-06 16:21:04 +0530

    Strings

--- JAVA/String/Methods.java
+++ JAVA/String/Methods.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class Methods {
     public static void main(String[] args) {
-        
+        String name="singh brar";
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toLowerCase());
+        System.out.println(name.indexOf('n'));
+        System.out.println("   singh     ".strip());
+        System.out.println(Arrays.toString(name.split(" ")));
     }    
 }
