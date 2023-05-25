0704d7131a42edb9654e4b51e1b46a37e14a749b
prabhjyot009 <prabh156dps@gmail.com>
2023-05-25 10:57:22 +0530

    java ques

--- JAVA/Array/CommonElements.java
+++ JAVA/Array/CommonElements.java
@@ -1,8 +1,8 @@
 public class CommonElements {
     public static void main(String[] args) {
         int[] arr1={1,2,3,4,5};
-        int[] arr2={1,2,3,4,5};
-        int[] arr3={1,2,3,4,5};
+        int[] arr2={0,0,1,2,3};
+        int[] arr3={1,2,3,0,0};
         CommonElements(arr1,arr2,arr3);
     }
     static void CommonElements(int[] arr1,int[] arr2,int[] arr3){
@@ -31,22 +31,22 @@ public class CommonElements {
 }
 //Big O n
 //now Big O of n cube:
-public class CommonElements {
-    public static void main(String[] args) {
-        int[] arr1={1,2,3,4,5};
-        int[] arr2={1,2,3,4,5};
-        int[] arr3={1,2,3,4,5};
-        CommonElements(arr1,arr2,arr3);
-    }
-    static void CommonElements(int[] arr1,int[] arr2,int[] arr3){
-        for (int i = 0; i < arr1.length; i++) {
-            for (int j = 0; j < arr2.length-1; j++) {
-                for (int k = 0; k < arr3.length-1; k++) {
-                    if(arr1[i]==arr2[j] && arr2[j]==arr3[k]){
-                        System.out.println(arr1[i]);
-                    }
-                }
-            }
-        }
-    }
-}
+// public class CommonElements {
+//     public static void main(String[] args) {
+//         int[] arr1={1,2,3,4,5};
+//         int[] arr2={1,2,3,4,5};
+//         int[] arr3={1,2,3,4,5};
+//         CommonElements(arr1,arr2,arr3);
+//     }
+//     static void CommonElements(int[] arr1,int[] arr2,int[] arr3){
+//         for (int i = 0; i < arr1.length; i++) {
+//             for (int j = 0; j < arr2.length-1; j++) {
+//                 for (int k = 0; k < arr3.length-1; k++) {
+//                     if(arr1[i]==arr2[j] && arr2[j]==arr3[k]){
+//                         System.out.println(arr1[i]);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
--- JAVA/Array/DutchRelationalLeetcode.java
+++ JAVA/Array/DutchRelationalLeetcode.java
@@ -1,17 +1,34 @@
-public class DutchRelationalLeetcode {
     public static void main(String[] args) {
         int[] arr={0,0,1,1,2,0};
-        //sort array
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]<arr[i-1]){
-                i=0;
-                arr[i]=arr[i]+arr[i-1];
-                arr[i-1]=arr[i]-arr[i-1];
-                arr[i]=arr[i]-arr[i-1];
+        int low=0;  //low pointer
+        int mid=0; //mid pointer
+        int high=arr.length-1; //high pointer
+        while(mid<=high){ //loop until mid is greater than high
+            if(arr[mid]==0){ //if mid is 0, swap low and mid, increment low and mid
+                int temp=arr[low];
+                arr[low]=arr[mid];
+
+                arr[mid]=temp;
+                low++;
+                mid++;
             }
-                }
-                for(int i=0;i<arr.length;i++){
-                    System.out.println(arr[i]);
-                }
+            else if(arr[mid]==1){ //if mid is 1, increment mid
+                mid++;
             }
+
+            else{ //if mid is 2, swap mid and high, decrement high
+                int temp=arr[mid];
+        
+                arr[mid]=arr[high];
+                arr[high]=temp;
+                high--;
+            }
+        }    
+        for(int i=0;i<arr.length;i++){ //print the array
+
+            System.out.println(arr[i]);
         }
+
+    }
+
+}
